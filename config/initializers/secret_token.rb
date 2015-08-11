cb8cabda1a613133dfa960a398095d530da6b968
pivotal <pivotal@thrift.sf.pivotallabs.com>
2015-08-11 11:07:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudFoodry::Application.config.secret_key_base = 'ecef3c0530332344f9d2d31c1cccc3889f49a6d0b178e04ed47341b3228b5e37f77e2984bbc6275e5f54db3c374bf06b0f9668845093e63adc3db82325eac0a6'
